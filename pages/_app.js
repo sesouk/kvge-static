4e69da9f5dffe5b657c630999263848253c2d9ac
Kevin Tran <sesouk@gmail.com>
2020-05-25 16:59:36 -0600

    fixed a typo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styling/home.css";
-import "../Styling/shop.css";
-import "../Styling/about.css";
-import '../Styling/cart.css';
+import "../styling/shop.css";
+import "../styling/about.css";
+import '../styling/cart.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
